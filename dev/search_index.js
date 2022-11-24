eebf33f1b9243d560068ee51857dfe75580e9d00
Documenter.jl <documenter@juliadocs.github.io>
2022-11-24 16:01:10 +0000

    build based on e8b368e

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#SLAM_test.jl","page":"Home","title":"SLAM_test.jl","text":"","category":"section"}]
+[{"location":"#BagFile.jl","page":"Home","title":"BagFile.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"CurrentModule = BagFile","category":"page"},{"location":"","page":"Home","title":"Home","text":"BagFile.jl is a Julia package for parsing ROS bag files","category":"page"},{"location":"","page":"Home","title":"Home","text":"The aim is to provide a library for working with ROS bags in the same way as the command line tool from ROS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"Pages = [\"index.md\"]","category":"page"}]
 }
